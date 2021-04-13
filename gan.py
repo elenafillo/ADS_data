9b8bf7bce117561ca1d01059fd527a6395f94968
unknown <tx17079@bristol.ac.uk>
2021-04-13 12:48:39 +0100

    rewrote discriminator

--- gan.py
+++ gan.py
@@ -70,32 +70,41 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
-            # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            # input is (nc) x 256 x 256
+            nn.Conv2d(nc, 8, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf) x 32 x 32
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
+            nn.MaxPool2d(2,1),
+            # state size. 8 x 128 x 128
+            nn.Conv2d(8,16, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(16),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*2) x 16 x 16
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
+            nn.MaxPool2d(2,1),
+            # state size. 16 x 64 x 64
+            nn.Conv2d(16,32, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
             nn.LeakyReLU(0.2, inplace=True),
-            # state size. (ndf*4) x 8 x 8
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
+            nn.MaxPool2d(2,1),
+            # state size. 32 x 32 x 32
+            nn.Conv2d(32,64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d(2,1),
+            #16x16x64
+            nn.Conv2d(64,128, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d(2,1),
+            #8x8x128
+            nn.Conv2d(128,256, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d(2,1),
+
+            nn.Flatten(),
+            nn.Linear(1024,128, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 16),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(ndf * 16, ndf * 32, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 32),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 32, 1, 4, 1, 0, bias=False),
+            nn.Linear(128,1, bias=False),
             nn.Sigmoid()
         )
 
@@ -157,7 +166,7 @@ def run():
     data=[]
     for f in os.listdir(path):
         # print(len(data))
-        if f.endswith(".npz") and "fg_cut" in f:
+        if f.endswith(".npz") and "aila_fg_cut" in f:
             print(f)
             datapoint=np.load(path+f, allow_pickle=True)
             for x in datapoint['arr_0']:
